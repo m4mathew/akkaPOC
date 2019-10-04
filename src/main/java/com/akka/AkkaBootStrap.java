cad1a90b33e1e70db1c6ab7c2d7f3146e11917c8
manmathew <v-manmathew@expedia.com>
2019-10-04 16:48:49 +0530

    thenAccept

--- src/main/java/com/akka/AkkaBootStrap.java
+++ src/main/java/com/akka/AkkaBootStrap.java
@@ -5,7 +5,9 @@ import com.akka.actors.ListenerActor;
 import com.akka.actors.MasterActor;
 import com.akka.messages.FindAddressReqMessage;
 
+import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 
 public class AkkaBootStrap {
@@ -18,17 +20,36 @@ public class AkkaBootStrap {
         ActorRef master = system.actorOf( Props.create(MasterActor.class,listener), "master");
 
         System.out.println("Actor Ref path: " + master.path().toString());
-       master.tell(new FindAddressReqMessage("Mathew"), ActorRef.noSender());
+        master.tell(new FindAddressReqMessage("Mathew"), ActorRef.noSender());
 
         //using selection
         ActorSelection selection = system.actorSelection("/user/master");
 
+        //System.out.println("## Using Selection ## path " + selection.pathString() );
 
-        System.out.println("## Using Selection ## path " + selection.pathString() );
+        //selection.tell(new FindAddressReqMessage("Max"),ActorRef.noSender());
 
-        selection.tell(new FindAddressReqMessage("Max"),ActorRef.noSender());
+
+        //thenAccept use
+        checkIfActorExists(system,"/user/master").thenAccept(actorRef -> actorRef.tell(new FindAddressReqMessage("Max"),ActorRef.noSender()));
+
+        // exceptionally use
+        checkIfActorExists(system,"/user/InValidActor");
+
 
     }
 
+    private static CompletionStage<ActorRef> checkIfActorExists(ActorSystem system, String path){
+        System.out.println("Check if Actor Exists");
+            ActorSelection selection = system.actorSelection(path);
+
+            //returns Actor Ref if Actor exists.
+            return selection.resolveOne(Duration.ofMillis(1000))
+                    .exceptionally(e->{
+                        System.out.println("Exception Occured  " + e.getMessage());
+                        return null;
+                    });
+
+    }
 
 }
